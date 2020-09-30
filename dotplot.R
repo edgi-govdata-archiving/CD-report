666fe0c2b6f0f9c74ac5d79eb059de4caa5e96ea
mraisle <mraisle@live.unc.edu>
2020-09-29 20:56:05 -0400

    edits

--- dotplot.R
+++ dotplot.R
@@ -1,34 +1,31 @@
-#Making the dot plot
+
+filenames = list.files(here("nationalstats"), pattern = ".csv")
+file_count = length(filenames)
+for (i in filenames){
+  name <- gsub("^([^_]*_[^_]*)_.*$","\\1", i)
+  assign(name, read.csv(here("nationalstats", i)))
+}
 
 
+CWAper1000 <- violationsper1000_CWA
+
+
+##dotplot for NY9 - replace the dotplot code in the .rmd with this for NY9
 
 CWAper1000$Num.per.1000 <- as.numeric(round(CWAper1000$Num.per.1000,0))
-CWAper1000$Group <- as.factor("Region")
+CWAper10002 <- CWAper1000
 CWAper10002$Test <- as.numeric(0)
-CWAper10002$Highlight <- ifelse(CWAper10002$CD.State=="VA4", "VA4", "Other")
-CWAper1000$Highlight <- ifelse(CWAper1000$CD.State=="VA4", "VA4", "Other")
+CWAper10002$Highlight <- ifelse(CWAper10002$CD.State=="NY9", "NY9", "Other")
+CWAper1000$Highlight <- ifelse(CWAper1000$CD.State=="NY9", "NY9", "Other")
+CWAper1000$Per100 <- as.numeric(CWAper1000$Num.per.1000/10)
 
 
 #make a dataframe that is per 100
 
 CWAper100 <- CWAper1000
-CWAper100$Per100 <- as.numeric(CWAper100$Num.per.1000/10)
 #removing NY9 and WA5 and MI7, AK, and IA2
-CWAper100 <- CWAper100[-c(1,21,39,51,70),]
+CWAper100 <- CWAper100[-c(38,52,29,15,54),]
 
-
-dotplot <-ggplot(CWAper100, aes(x=Group, y=Per100, fill=Highlight))+
-  geom_dotplot(binaxis='y', stackdir='centerwhole', stackratio=1.5, binwidth=15, dotsize=1.5 )+
-  geom_hline(yintercept=100, linetype="dashed", color="#e56d13")+
-  scale_fill_manual(values=c("#ffffff","#569b5e"))+
-  labs(title="CWA Violations per 100 Facilities", x="CD/State", y="")+
-  geom_text(aes(label="1 violation for every facility", family="Georgia"),
-            x=1.4, y=130, color="#e56d13", size=4)+
-  theme_meg()+
-  theme(axis.text.x=element_blank(), axis.line.x=element_blank(), axis.title.x=element_blank(),
-        legend.title = element_blank())
-
-dotplot
 
 
 dotplot <-ggplot(CWAper100, aes(x=Region, y=Per100, fill=Highlight))+
@@ -42,40 +39,119 @@ dotplot <-ggplot(CWAper100, aes(x=Region, y=Per100, fill=Highlight))+
             x=2.35, y=180, color="#e56d13", size=3)+
   geom_text(aes(label="0 violations", family="Georgia"),
             x=2.4, y=15, color="#569b5e", size=3)+
-  geom_label(aes(family="Georgia"),label="Outliers:\nNY9-2650 violations\nWA5-1029 violatns\nMI7-865 violations\nAK-841 violations\nIA2-826 violations",
-            x=2.3, y=600, color="black", size=3, show.legend = FALSE)+
+  annotate("text", label="Outliers:",
+             x=2.3, y=600, color="black", size=3, family="Georgia")+
+  annotate("text", label="NY9-2650 violations",
+           x=2.3, y=573, color="#e56d13", size=3, family="Georgia", fontface="bold")+
+  annotate("text", label="WA5-1029 violations\nMI7-865 violations\nAK-841 violations\nIA2-826 violations",
+           x=2.3, y=480, color="black", size=3, family="Georgia")+
   theme_meg()+
   theme(axis.line.x=element_blank(), axis.title.x=element_blank(),
         legend.title = element_blank(), axis.text.x=element_text(size=10), legend.position = c(0.1,.8))
 
 dotplot
 
-check_overlap = TRUE, nudge_x=.02
 
-geom_text(aes(label=CD.State, family="Georgia"),
-          position = position_dodge(0.9), vjust = -0.8, hjust=.5,size=3)+
+#WA5
 
-CWAper10002$Highlight <- ifelse(CWAper10002$CD.State=="VA4", "VA4", "other")
-CWAper1000$Highlight <- ifelse(CWAper1000$CD.State=="VA4", "VA4", "other")
-
-
-dotplot2 <-ggplot(CWAper10002, aes(x=Group, y=Num.per.1000, fill=Highlight)) +
-  geom_point()+
-  scale_fill_manual(values=c("black","#569b5e"))+
-  labs(title="CWA Violations per 1000 Facilities", x="CD/State", y="")+
-  geom_text(aes(label=CD.State, family="Georgia"),
-            position = position_dodge(0.9), vjust = -0.8, hjust=.2,size=3)+
+dotplot <-ggplot(CWAper100, aes(x=Region, y=Per100, fill=Highlight))+
+  geom_dotplot(binaxis='y', stackdir='centerwhole', stackratio=2.5, binwidth=15,
+               dotsize=2)+
+  geom_hline(yintercept=100, linetype="dashed", color="#e56d13")+
+  geom_hline(yintercept=0, linetype="dashed", color="#569b5e")+
+  scale_fill_manual(values=c("#ffffff","#e56d13"))+
+  labs(title="CWA Violations per 100 Facilities", x="CD/State", y="")+
+  geom_text(aes(label="100 violations:\n1:1 ratio of violations\nto # of facilities", family="Georgia"),
+            x=2.35, y=180, color="#e56d13", size=3)+
+  geom_text(aes(label="0 violations", family="Georgia"),
+            x=2.4, y=15, color="#569b5e", size=3)+
+  annotate("text", label="Outliers:\nNY9-2650 violations",
+           x=2.3, y=600, color="black", size=3, family="Georgia")+
+  annotate("text", label="WA5-1029 violations",
+           x=2.3, y=550, color="#e56d13", size=3, family="Georgia", fontface="bold")+
+  annotate("text", label="MI7-865 violations\nAK-841 violations\nIA2-826 violations",
+           x=2.3, y=480, color="black", size=3, family="Georgia")+
   theme_meg()+
-  theme(axis.text.x=element_blank(), axis.line.x=element_blank())
+  theme(axis.line.x=element_blank(), axis.title.x=element_blank(),
+        legend.title = element_blank(), axis.text.x=element_text(size=10), legend.position = c(0.1,.8))
 
-dotplot2
+ggsave("dotplot_WA5.png", dotplot, path=here("outlierdotplots"), scale = 2, width=8, height=5, units=c("cm"))
 
-,
-position = position_dodge(0.9), vjust = -0.1, size=3)+
 
-p
-# Change dotsize and stack ratio
-ggplot(ToothGrowth, aes(x=dose, y=len)) +
-  geom_dotplot(binaxis='y', stackdir='center',
-               stackratio=1.5, dotsize=1.2)
+#MI7
+
+dotplot <-ggplot(CWAper100, aes(x=Region, y=Per100, fill=Highlight))+
+  geom_dotplot(binaxis='y', stackdir='centerwhole', stackratio=2.5, binwidth=15,
+               dotsize=2)+
+  geom_hline(yintercept=100, linetype="dashed", color="#e56d13")+
+  geom_hline(yintercept=0, linetype="dashed", color="#569b5e")+
+  scale_fill_manual(values=c("#ffffff","#e56d13"))+
+  labs(title="CWA Violations per 100 Facilities", x="CD/State", y="")+
+  geom_text(aes(label="100 violations:\n1:1 ratio of violations\nto # of facilities", family="Georgia"),
+            x=2.35, y=180, color="#e56d13", size=3)+
+  geom_text(aes(label="0 violations", family="Georgia"),
+            x=2.4, y=15, color="#569b5e", size=3)+
+  annotate("text", label="Outliers:\nNY9-2650 violations\nWA5-1029 violations",
+           x=2.3, y=600, color="black", size=3, family="Georgia")+
+  annotate("text", label="MI7-865 violations",
+           x=2.3, y=525, color="#e56d13", size=3, family="Georgia", fontface="bold")+
+  annotate("text", label="AK-841 violations\nIA2-826 violations",
+           x=2.3, y=470, color="black", size=3, family="Georgia")+
+  theme_meg()+
+  theme(axis.line.x=element_blank(), axis.title.x=element_blank(),
+        legend.title = element_blank(), axis.text.x=element_text(size=10), legend.position = c(0.1,.8))
+
+ggsave("dotplot_MI7.png", dotplot, path=here("outlierdotplots"), scale = 2, width=8, height=5, units=c("cm"))
+
+
+#AK
+dotplot <-ggplot(CWAper100, aes(x=Region, y=Per100, fill=Highlight))+
+  geom_dotplot(binaxis='y', stackdir='centerwhole', stackratio=2.5, binwidth=15,
+               dotsize=2)+
+  geom_hline(yintercept=100, linetype="dashed", color="#e56d13")+
+  geom_hline(yintercept=0, linetype="dashed", color="#569b5e")+
+  scale_fill_manual(values=c("#ffffff","#e56d13"))+
+  labs(title="CWA Violations per 100 Facilities", x="CD/State", y="")+
+  geom_text(aes(label="100 violations:\n1:1 ratio of violations\nto # of facilities", family="Georgia"),
+            x=2.35, y=180, color="#e56d13", size=3)+
+  geom_text(aes(label="0 violations", family="Georgia"),
+            x=2.4, y=15, color="#569b5e", size=3)+
+  annotate("text", label="Outliers:\nNY9-2650 violations\nWA5-1029 violations\nMI7-865 violations",
+           x=2.3, y=600, color="black", size=3, family="Georgia")+
+  annotate("text", label="AK-841 violations",
+           x=2.3, y=505, color="#e56d13", size=3, family="Georgia", fontface="bold")+
+  annotate("text", label="IA2-826 violations",
+           x=2.3, y=467, color="black", size=3, family="Georgia")+
+  theme_meg()+
+  theme(axis.line.x=element_blank(), axis.title.x=element_blank(),
+        legend.title = element_blank(), axis.text.x=element_text(size=10), legend.position = c(0.1,.8))
+
+ggsave("dotplot_AK.png", dotplot, path=here("outlierdotplots"), scale = 2, width=8, height=5, units=c("cm"))
+
+
+
+#IA2
+dotplot <-ggplot(CWAper100, aes(x=Region, y=Per100, fill=Highlight))+
+  geom_dotplot(binaxis='y', stackdir='centerwhole', stackratio=2.5, binwidth=15,
+               dotsize=2)+
+  geom_hline(yintercept=100, linetype="dashed", color="#e56d13")+
+  geom_hline(yintercept=0, linetype="dashed", color="#569b5e")+
+  scale_fill_manual(values=c("#ffffff","#e56d13"))+
+  labs(title="CWA Violations per 100 Facilities", x="CD/State", y="")+
+  geom_text(aes(label="100 violations:\n1:1 ratio of violations\nto # of facilities", family="Georgia"),
+            x=2.35, y=180, color="#e56d13", size=3)+
+  geom_text(aes(label="0 violations", family="Georgia"),
+            x=2.4, y=15, color="#569b5e", size=3)+
+  annotate("text", label="Outliers:\nNY9-2650 violations\nWA5-1029 violations\nMI7-865 violations\nAK-841 violations",
+           x=2.3, y=600, color="black", size=3, family="Georgia")+
+  annotate("text", label="IA2-826 violations",
+           x=2.3, y=485, color="#e56d13", size=3, family="Georgia", fontface="bold")+
+  theme_meg()+
+  theme(axis.line.x=element_blank(), axis.title.x=element_blank(),
+        legend.title = element_blank(), axis.text.x=element_text(size=10), legend.position = c(0.1,.8))
+
+ggsave("dotplot_IA2.png", dotplot, path=here("outlierdotplots"), scale = 2, width=8, height=5, units=c("cm"))
+
+
+
 
